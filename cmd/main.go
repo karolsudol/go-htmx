84cc81d0d318a9faf785db127a87c0176abd2b45
karolsudol <karol@sudoltrust.com>
2024-05-20 22:22:27 +0100

    render piece=html

--- cmd/main.go
+++ cmd/main.go
@@ -36,12 +36,12 @@ func main() {
 	e.Use(middleware.Logger())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index.html", count)
+		return c.Render(200, "index", count)
 	})
 
 	e.POST("/count", func(c echo.Context) error {
 		count.Count++
-		return c.Render(200, "index.html", count)
+		return c.Render(200, "count", count)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
