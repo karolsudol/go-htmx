5c1b28ddcd3238313cbda152029345ce1c3f9206
karolsudol <karol@sudoltrust.com>
2024-05-21 21:35:58 +0100

    contacts

--- cmd/main.go
+++ cmd/main.go
@@ -22,26 +22,48 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+type Data struct {
+	Contacts []Contact
+}
+
+func NewData() *Data {
+	return &Data{
+		Contacts: []Contact{},
+	}
+}
+
+func NewContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
 }
 
 func main() {
 
 	e := echo.New()
 
-	count := Count{Count: 0}
+	data := NewData()
 
 	e.Renderer = newTemplate()
 	e.Use(middleware.Logger())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index.html", data)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		data.Contacts = append(data.Contacts, NewContact(name, email))
+
+		return c.Render(200, "index.html", data)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
